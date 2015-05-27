2af9a98c2590399508e1ebbc12f299ba2e8b8ef7
phonger <vupt@yahoo.com>
2015-05-27 15:06:16 -0600

    00_initial

---
+++ app/src/main/java/pee/mockbanking/LoginActivity.java
@@ -0,0 +1,114 @@
+package pee.mockbanking;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LoginActivity extends Activity {
+
+    private static final String TAG = "LoginActivity";
+    private EditText username;
+    private EditText password;
+    private Button login;
+    private TextView loginLockedTV;
+    private TextView attemptsLeftTV;
+    private TextView numberOfRemainingLoginAttemptsTV;
+    int numberOfRemainingLoginAttempts = 3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Calling Application class (see application tag in AndroidManifest.xml)
+        final AppSession appSession = (AppSession) getApplicationContext();
+
+        //Set name and email in global/application context
+        appSession.setUserName("maivo");
+        appSession.setPassword("test");
+        setupVariables();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.action_go) {
+            handleGoSecondActivity();
+            return true;
+        }
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void handleGoSecondActivity(){
+        Log.i(TAG, "inside handleGoSecondActivity");
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        // calling an activity using <intent-filter> action name
+        //  Intent intent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
+
+        startActivity(intent);
+
+
+    }
+
+    private void setupVariables() {
+        username = (EditText) findViewById(R.id.usernameET);
+        password = (EditText) findViewById(R.id.passwordET);
+        login = (Button) findViewById(R.id.loginBtn);
+        loginLockedTV = (TextView) findViewById(R.id.loginLockedTV);
+        attemptsLeftTV = (TextView) findViewById(R.id.attemptsLeftTV);
+        numberOfRemainingLoginAttemptsTV = (TextView) findViewById(R.id.numberOfRemainingLoginAttemptsTV);
+        numberOfRemainingLoginAttemptsTV.setText(Integer.toString(numberOfRemainingLoginAttempts));
+    }
+
+    public void authenticateLogin(View view) {
+        if (username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Hello admin!",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Seems like you 're not admin!",
+                    Toast.LENGTH_SHORT).show();
+            numberOfRemainingLoginAttempts--;
+            attemptsLeftTV.setVisibility(View.VISIBLE);
+            numberOfRemainingLoginAttemptsTV.setVisibility(View.VISIBLE);
+            numberOfRemainingLoginAttemptsTV.setText(Integer.toString(numberOfRemainingLoginAttempts));
+
+            if (numberOfRemainingLoginAttempts == 0) {
+                login.setEnabled(false);
+                loginLockedTV.setVisibility(View.VISIBLE);
+                loginLockedTV.setBackgroundColor(Color.RED);
+                loginLockedTV.setText("LOGIN LOCKED!!!");
+            }
+        }
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+hello_login
+
+follow along this tutorial to create login screen
+http://examples.javacodegeeks.com/android/android-login-example/
